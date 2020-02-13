882b5afe7ce81f525ff95a53aea69bc546f139ac
KenKurose <51434724+KentKurose@users.noreply.github.com>
2020-02-13 16:28:52 +0900

    Simple Logical Circuit
    Simple Logical Circuit

---
+++ 2.2.LogicalCircuit.py
@@ -0,0 +1,123 @@
+import numpy as np
+
+
+# 2.2 単純な論理回路
+
+# 2.3 パーセプトロンの実装
+
+# 2.3.1 簡単な実装
+'''
+def AND(x1,x2):
+    w1,w2,theta=0.5,0.5,0.7
+    tmp=x1+w1+x2*w2
+    if tmp <= theta:
+        return 0
+    elif tmp > theta:
+        return 1
+
+print("AND(0,0)")
+print(AND(0,0))
+
+print("AND(0,1)")
+print(AND(0,1))
+
+print("AND(1,0)")
+print(AND(1,0))
+
+print("AND(1,1)")
+print(AND(1,1))
+'''
+
+# 2.3.2 バイアス
+
+'''
+x=np.array([0,1]) #入力
+w=np.array([0.5,0.5]) #重み
+b=-0.7 # バイアス
+
+print(x)
+print(w)
+
+tmp=x*w
+print(tmp)
+
+sum=np.sum(tmp)
+print(sum)
+
+print(sum+b)
+'''
+
+
+# 2.3.3 重みとバイアスによるAND実装
+
+def AND(x1,x2):
+    x=np.array([x1,x2])
+    w=np.array([0.5,0.5])
+    b=-0.7
+    tmp=np.sum(w*x)+b
+
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def NAND(x1,x2):
+    x=np.array([x1,x2])
+    w=np.array([-0.5,-0.5])
+    b=0.7
+    tmp=np.sum(w*x)+b
+
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def OR(x1,x2):
+    x=np.array([x1,x2])
+    w=np.array([0.5,0.5])
+    b=-0.2
+    tmp=np.sum(w*x)+b
+
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+print("AND(0,0)")
+print(AND(0,0))
+
+print("AND(0,1)")
+print(AND(0,1))
+
+print("AND(1,0)")
+print(AND(1,0))
+
+print("AND(1,1)")
+print(AND(1,1))
+
+
+print("NAND(0,0)")
+print(NAND(0,0))
+
+print("NAND(0,1)")
+print(NAND(0,1))
+
+print("NAND(1,0)")
+print(NAND(1,0))
+
+print("NAND(1,1)")
+print(NAND(1,1))
+
+
+print("OR(0,0)")
+print(OR(0,0))
+
+print("OR(0,1)")
+print(OR(0,1))
+
+print("OR(1,0)")
+print(OR(1,0))
+
+print("OR(1,1)")
+print(OR(1,1))
