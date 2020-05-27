92ce32bacb2f890c17f3f8365074da5655387f3e
kentkurose <kurose.kentaro.kk@jp-rissa.org>
2020-05-27 10:28:35 +0900

    Updated

--- 3.6.1.MNISTDataSet.py
+++ 3.6.1.MNISTDataSet.py
@@ -4,3 +4,17 @@
 Download from
 https://github.com/oreilly-japan/deep-learning-from-scratch
 '''
+
+import os
+import sys
+
+sys.path.append(os.pardir)
+from dataset.mnist import load_mnist
+
+# takes a few minuets
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+
+print(x_train.shape)
+print(t_train.shape)
+print(x_test.shape)
+print(t_test.shape)
